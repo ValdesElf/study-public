c47d542538882650ac54d81af2b418553e68a2b5
ValdesElf <valdeself@gmail.com>
2024-03-10 20:56:08 +0300

    sos

--- мониторинг товаров/js/script.js
+++ мониторинг товаров/js/script.js
@@ -15,20 +15,82 @@
 
 const button = document.querySelector("#asideActiveBtn")
 const aside = document.querySelector('.aside')
+const subscriptionArray = [
+    {
+      status: 'new',
+      email: 'pateder@yandex.ru',
+      productId: 23423423,
+      variantId: 45235646,
+      title: 'Джинсы Бойфренд S',
+      createAt: new Date('24.10.2023')
+    },
+    {
+      status: 'new',
+      email: 'pateder@yandex.ru',
+      productId: 23423423,
+      variantId: 45235646,
+      title: 'Джинсы Бойфренд S',
+      createAt: new Date('25.11.2023')
+    },
+    {
+      status: 'new',
+      email: 'pateder@yandex.ru',
+      productId: 23423423,
+      variantId: 45235646,
+      title: 'Джинсы Бойфренд S',
+      createAt: new Date('13.12.2023')
+    },
+    {
+      status: 'new',
+      email: 'pateder@yandex.ru',
+      productId: 23423423,
+      variantId: 45235646,
+      title: 'Джинсы Бойфренд S',
+      createAt: new Date('12.11.2023')
+    },
+    {
+      status: 'new',
+      email: 'pateder@yandex.ru',
+      productId: 23423423,
+      variantId: 45235646,
+      title: 'Джинсы Бойфренд S',
+      createAt: new Date('12.12.2022')
+    }
+]
+subscriptionArray.forEach(function(item) {
+  let row = document.createElement('tr')
+  row.innerHTML = `
+    subscription__item
+  `
+})
+
+
+const container = document.querySelector('.subscription__item');
+  // const html = subscriptionArray.map(i => { return '<div>$(i)</div>'}).join('')
+  // container.innerHTML += html
+
 
 
 button.addEventListener('click', function()  {
-    aside.classList.toggle('aside_active')
-    // if (aside.classList.contains('aside_active')) {
-    //     aside.classList.remove('aside_active')
-    // } else {
-    //     aside.classList.add("aside_active")
-    // }
+    // aside.classList.toggle('aside_active')
+    if (aside.classList.contains('aside_active')) {
+        localStorage.setItem('isOpen', 'false')
+        aside.classList.remove('aside_active')
+    } else {
+        localStorage.setItem('isOpen', 'true')
+        aside.classList.add("aside_active")
+    }
 });
 
+const localStorageEvent = function() {
+    if (localStorage.getItem('isOpen') === 'false') {
+        aside.classList.remove('aside_active')
+    } else {
+        aside.classList.add("aside_active")
+    }
+}
 
-
-
+localStorageEvent()
 
 // document.querySelector(".info").addEventListener('click', function()  {
 //     if (document.querySelector('.aside').contains('.aside_active')) {
